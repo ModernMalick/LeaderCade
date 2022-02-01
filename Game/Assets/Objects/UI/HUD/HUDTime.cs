e1fdc7674473a0e394fd8561dd8be82720f64455
Malick NDIAYE <malick1ndiaye0@gmail.com>
2022-02-01 17:06:54 +0100

    Design - Gameplay done

--- Game/Assets/Objects/UI/HUD/HUDTime.cs
+++ Game/Assets/Objects/UI/HUD/HUDTime.cs
@@ -1,20 +1,92 @@
-using TMPro;
+using System;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 namespace Objects.UI.HUD
 {
     public class HUDTime : MonoBehaviour
     {
-        private TextMeshProUGUI _textMeshPro;
+        [Header("COLORS")]
+        [SerializeField] private Color green;
+        [SerializeField] private Color orange;
+        [SerializeField] private Color red;
+        
+        [Header("OBJECTS")]
+        [SerializeField] private GameObject gameObjectLeft;
+        [SerializeField] private GameObject gameObjectCenter;
+        [SerializeField] private GameObject gameObjectRight;
+
+        private Image _left;
+        private Image _center;
+        private Image _right;
+        
+        private Image[] _images;
+        private GameObject[] _gameObjects;
+        private float _time;
 
         private void Start()
         {
-            _textMeshPro = gameObject.GetComponent<TextMeshProUGUI>();
+            _gameObjects = new[] { gameObjectLeft, gameObjectCenter, gameObjectRight };
+            
+            _left = gameObjectLeft.GetComponent<Image>();
+            _center = gameObjectCenter.GetComponent<Image>();
+            _right = gameObjectRight.GetComponent<Image>();
+            _images = new[] { _left, _center, _right };
         }
 
         private void Update()
         {
-            _textMeshPro.text = TimeManager.Instance.RemainingTime.ToString("0");
+            _time = TimeManager.Instance.RemainingTime;
+            UpdateTime();
+        }
+
+        private void UpdateTime()
+        {
+            Color color;
+
+            if (CheckTime(3))
+            {
+                color = green;
+                foreach (var i in _gameObjects)
+                {
+                    i.SetActive(true);
+                }
+            } 
+            else if (CheckTime(2))
+            {
+                color = orange;
+                gameObjectRight.SetActive(false);
+            } 
+            else if (CheckTime(1))
+            {
+                color = red;
+                gameObjectCenter.SetActive(false);
+            }            
+            else if (CheckTime(0))
+            {
+                gameObjectLeft.SetActive(false);
+                return;
+            }
+            else
+            {
+                return;
+            }
+            
+            UpdateColor(color);
+        }
+
+        private bool CheckTime(int comparer)
+        {
+            return Math.Abs(_time - comparer) < 0.1;
+        }
+
+        private void UpdateColor(Color color)
+        {
+            foreach (var image in _images)
+            {
+                image.color = color;
+            }
         }
     }
 }
